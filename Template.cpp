85d2bc038877e9762b6772da1e7b5cc2c5e95223
NidhishKumarReddy <nidhishkumarreddy@gmail.com>
2020-05-10 16:07:55 +0530

    updated template.cpp

---
+++ Template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int		long long
+using namespace std;
+
+void c_p_c(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r",stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("debug.txt", "w", stderr);
+#endif
+}
+
+int32_t main(){
+	c_p_c();
+	return 0;
+}
+
+// g++ -std=c++17 -Wshadow -Wall -o "Template" "Template.cpp" -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG
