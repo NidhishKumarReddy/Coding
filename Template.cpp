eee8f6491f445e027f2791214428e910c1129da8
NidhishKumarReddy <nidhishkumarreddy@gmail.com>
2020-05-10 22:20:22 +0530

    modified template

--- Template.cpp
+++ Template.cpp
@@ -1,22 +1,12 @@
 #include <bits/stdc++.h>
 #define int	long long
 #define sz(x)	((int)(x).size())
-#define w(x)	int x; cin>>x; while(x--)
 #define db(x)	{ cerr << #x << " = " << x << endl; }
 using namespace std;
-
-void c_p_c(){
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r",stdin);
-	freopen("output.txt", "w", stdout);
-	freopen("debug.txt", "w", stderr);
-#endif
-}
+void c_p_c(){ios::sync_with_stdio(0); cin.tie(0);}
 
 int32_t main(){
 	c_p_c();
 	return 0;
 }
-
 // g++ -std=c++17 -Wshadow -Wall -o "Template" "Template.cpp" -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG
