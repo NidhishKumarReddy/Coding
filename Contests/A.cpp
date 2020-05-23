378f60e44bc93b7252d2a317e113a6568035f7a9
NidhishKumarReddy <nidhishkumarreddy@gmail.com>
2020-05-23 14:23:30 +0530

    added newfiles

---
+++ Contests/A.cpp
@@ -0,0 +1,17 @@
+/******************************************
+* AUTHOR : sema_4 *
+******************************************/
+#include <bits/stdc++.h>
+using namespace std;
+#define int		long long
+
+void solve (){
+	cout<<"hello";
+}
+
+int32_t main(){
+	ios::sync_with_stdio(false); cin.tie(0);
+	int test; cin>>test;
+	while(test--){solve();}
+	return 0;
+}
